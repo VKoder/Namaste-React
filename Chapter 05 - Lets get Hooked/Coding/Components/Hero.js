ba519379c62b4b94d8de4a5f8baa76c16c682551
VKoder <vivekkhule204@gmail.com>
2023-11-18 12:51:23 +0530

    chapt 5

--- Chapter 05 - Lets get Hooked/Coding/Components/Hero.js
+++ Chapter 05 - Lets get Hooked/Coding/Components/Hero.js
@@ -2,33 +2,59 @@ import Card from "./Card";
 import { useState } from "react";
 import { restrauntData } from "../Utils/mockData";
 
-
 // HERO COMPONENT
 const Hero = () => {
-
   const [restrauntList, setrestrauntList] = useState(restrauntData);
-  
-  return(
-  <div className="hero">
-    <div className="search">
-      <input type="text" placeholder="Feeling Hungry?" id="searchInp"></input>
-      <button id="searchBtn">Search</button>
-      <button id="filter-btn" 
-            onClick={()=>{
-              let filteredList = restrauntList.filter((rest => rest.info.avgRating > 4.2));
-               {setrestrauntList(filteredList)}
-            }}>
-           
-            
-      Top Rated Restraunt</button>
+
+  return (
+    <div className="hero">
+      <div className="search">
+        <input type="text" placeholder="Feeling Hungry?" id="searchInp"></input>
+        <button id="searchBtn">Search</button>
+
+        <button
+          id="filter-btn"
+          onClick={() => {
+            let filteredList = restrauntList.filter(
+              (rest) => rest.info.avgRating > 4.2
+            );
+            {
+              setrestrauntList(filteredList);
+            }
+          }}
+        >
+          Top Rated Restraunt
+        </button>
+        <button
+          id="dis-btn"
+          onClick={() => {
+            let delivery = restrauntList.filter(
+              (rest) => rest.info.sla.deliveryTime < 20
+            );
+            {
+              setrestrauntList(delivery);
+            }
+          }}
+        >
+          Top Discount
+        </button>
+        <button
+          id="reset"
+          onClick={() => {
+            setrestrauntList(restrauntData);
+          }}
+        >
+          Reset
+        </button>
+      </div>
+      <div className="restraunt">
+        {
+          restrauntList.map((resItem) => (
+            <Card resData={resItem} />
+          )) // Iteratedly store restraunt Obj into resData which is a parameter of card!
+        }
+      </div>
     </div>
-    <div className="restraunt">
-      {
-        restrauntList.map((resItem) => (
-          <Card resData={resItem} />
-        )) // Iteratedly store restraunt Obj into resData which is a parameter of card!
-      }
-    </div>
-  </div>
-)};
+  );
+};
 export default Hero;
