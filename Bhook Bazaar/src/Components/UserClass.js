58da10ac6aa59843b2b0ae88a2d9033cf79a967a
VKoder <vivekkhule204@gmail.com>
2023-11-27 23:35:51 +0530

    user class

--- Bhook Bazaar/src/Components/UserClass.js
+++ Bhook Bazaar/src/Components/UserClass.js
@@ -1,12 +1,26 @@
 import React from "react"
+
+
 class UserClass extends React.Component{
     constructor(props){
         super(props)
+
+        this.state = {
+            count: 0,
+        }
     }
     render(){
-        const {name, location} = this.props
+        const {name, location} = this.props;
+        const {count} = this.state
         return (
             <div className="user-class">
+                <h2>Count: {count}</h2>
+                <button onClick={()=>{3
+                    this.setState=({
+                        count: this.state.count ++,
+                    });
+                    console.log(count)
+                }}>Increment</button>
                 <h1>Name: {name}</h1>
                 <h2>Location: {location}</h2>
             </div>
