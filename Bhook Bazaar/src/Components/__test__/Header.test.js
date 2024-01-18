993b2f65c64aa847ec424e3c4c70852aa736610d
VKoder <vivekkhule204@gmail.com>
2024-01-18 17:04:13 +0530

    testing in progress

--- Bhook Bazaar/src/Components/__test__/Header.test.js
+++ Bhook Bazaar/src/Components/__test__/Header.test.js
@@ -1,9 +1,18 @@
 import { render, screen } from "@testing-library/react"
 import Header from "../Header"
+import { Provider } from "react-redux"
+import appStore from "../../Redux Store/appStore"
+import { BrowserRouter } from "react-router-dom"
 
 
 test("Should have a logo",()=>{
-    render(<Header/>)
+    render(
+    <BrowserRouter>
+    <Provider store={appStore}>
+        <Header/>
+    </Provider>
+    </BrowserRouter>
+    )
 
     const about = screen.getByText("About")
     expect(about).toBeInTheDocument();
